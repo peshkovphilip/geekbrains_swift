71ee270bf332ae24ee1b28be2cabb0955beb9a97
peshkovphilip <peshkov.philip@gmail.com>
2019-07-03 16:09:26 +0300

    second lesson

--- second part/Swift Lesson 1/Swift Lessons/ViewController.swift
+++ second part/Swift Lesson 1/Swift Lessons/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var loginField: UITextField!
-    @IBOutlet weak var PasswordField: UITextField!
+    @IBOutlet var loginField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +20,49 @@ class ViewController: UIViewController {
     }
     
     @IBAction func LoginBtnTouch(_ sender: Any) {
-        print("username: \(loginField.text)")
-        print("password: \(PasswordField.text)")
+        print("username: \(loginField.text ?? "")")
+        print("username: \(passwordField.text ?? "")")
+    }
+
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "LoginSegue",
+            loginField.text == "admin",
+            passwordField.text == "123" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc private func keyboardWasShown(notification: Notification) {
+        let info = notification.userInfo as NSDictionary?
+        let keyboardSize = (info?.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc private func keyboardWasHidden(notification: Notification) {
+        
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
     }
 }
 
