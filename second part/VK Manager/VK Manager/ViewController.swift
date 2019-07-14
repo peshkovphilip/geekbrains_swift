124a0d155e1c1edd60b40e281896b7ab4663ed76
peshkovphilip <peshkov.philip@gmail.com>
2019-07-14 15:08:03 +0300

    lesson 3

---
+++ second part/VK Manager/VK Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Manager
+//
+//  Created by Филипп on 10/07/2019.
+//  Copyright © 2019 Филипп Пешков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
